e6d34d32b9f329d0649708b8b31f3c20d22e1c12
binarycraft007 <elliot.huang.signed@gmail.com>
2024-01-21 15:08:14 +0800

    build system update

--- build.zig
+++ build.zig
@@ -4,26 +4,27 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const libnl_dep = b.dependency("libnl_tiny", .{});
+
     const lib = b.addStaticLibrary(.{
         .name = "nl-tiny",
         .target = target,
         .optimize = optimize,
     });
-    lib.linkLibC();
-    lib.link_gc_sections = false;
-    lib.defineCMacro("_GNU_SOURCE", null);
     inline for (libnl_tiny_src) |src| {
         lib.addCSourceFile(.{
             .file = libnl_dep.path(src),
             .flags = &.{},
         });
     }
-    lib.addCSourceFile(.{
-        .file = .{ .path = "src/genl.c" },
-        .flags = &.{},
-    });
+    lib.linkLibC();
+    lib.root_module.addCMacro("_GNU_SOURCE", "");
     lib.addIncludePath(libnl_dep.path("include"));
-    lib.installHeadersDirectory(libnl_dep.path("include").getPath(b), "");
+    lib.installHeadersDirectoryOptions(.{
+        .source_dir = libnl_dep.path("include"),
+        .install_dir = .header,
+        .install_subdir = "",
+        .include_extensions = &.{".h"},
+    });
     b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{
@@ -31,7 +32,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib_unit_tests.linkLibrary(lib);
+    lib_unit_tests.root_module.linkLibrary(lib);
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
